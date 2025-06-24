bfc517ee92c6d7c3f9d92444e2aeb10a2e111ccd
Tumanov Artem <arti.msk2011@gmail.com>
2025-06-24 23:18:42 +0300

    init commit

---
+++ src/main/java/com/stepup/alp/MainApplication.java
@@ -0,0 +1,8 @@
+package com.stepup.alp;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Случайное число от 0 до 1: " + Math.random());
+    }
+}
