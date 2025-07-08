712690ed2f5a87003b841c71ce4231a15757abac
Tumanov Artem <arti.msk2011@gmail.com>
2025-07-08 14:56:57 +0300

    MAIN. MODULE 3. TASK 2. Implemented corrections.

--- src/main/java/com/stepup/alp/MainApplication.java
+++ src/main/java/com/stepup/alp/MainApplication.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class MainApplication {
     public static void main(String[] args) {
         for (int i = 0; i <= 5;i++ ) {
-            System.out.println("Введите число: ");
+            System.out.println("Введите первое число: ");
             int firstNumber = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число: ");
             int secondNumber = new Scanner(System.in).nextInt();
-            double result = (double) firstNumber / secondNumber;
-            System.out.println("Результат деления чисел: " + result);
+
+            //сумма
+            int intResult = firstNumber + secondNumber;
+            System.out.println("Результат сложения чисел (сумма): " + intResult);
+
+            //разность
+            intResult = firstNumber - secondNumber;
+            System.out.println("Результат вычитания чисел (разность): " + intResult);
+
+            //произведение
+            intResult = firstNumber * secondNumber;
+            System.out.println("Результат умножение чисел (произведение): " + intResult);
+
+            //частное
+            double doubleResult = (double) firstNumber / secondNumber;
+            System.out.println("Результат деления чисел (частное): " + doubleResult);
+
+            System.out.println("Расчет закончен!");
         }
     }
 }
