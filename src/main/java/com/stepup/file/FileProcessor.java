35efbfa9b8aac110f18e1dbac4f9ba45c4f95b5a
Tumanov Artem <arti.msk2011@gmail.com>
2025-08-14 22:19:19 +0300

    Removed obsolete commented code from FileProcessor.java

--- src/main/java/com/stepup/file/FileProcessor.java
+++ src/main/java/com/stepup/file/FileProcessor.java
@@ -26,72 +26,6 @@ public class FileProcessor {
         strategies.add(strategy);
     }
 
-//    public void processFile(String filePath) {
-//        parsedEntries = parseFile(filePath);
-//        for (ProcessingStrategy strategy : strategies) {
-//            strategy.processEntries(parsedEntries);
-//        }
-//    }
-
-//public void processFile(String filePath) {
-//    try {
-//        List<LogEntry> entries = parseFile(filePath);
-//
-//        if (entries.isEmpty()) {
-//            System.out.println("Нет записей для обработки");
-//            return;
-//        }
-//
-//        entries.stream()
-//                .peek(entry ->
-//                        strategies.forEach(strategy -> {
-//                            try {
-//                                strategy.processEntries(entry);
-//                            } catch (Exception e) {
-//                                System.err.println("Ошибка в стратегии " + strategy.getClass().getSimpleName());
-//                            }
-//                        })
-//                )
-//                .count(); // терминальная операция
-//    } catch (Exception e) {
-//        System.err.println("Ошибка при обработке файла: " + e.getMessage());
-//    }
-//}
-    //старый метод parseFile
-//    private List<LogEntry> parseFile(String filePath) {
-//        long startTime = System.nanoTime();
-//
-//        List<LogEntry> entries = new ArrayList<>();
-//        System.out.println("Доступно процессоров: " + Runtime.getRuntime().availableProcessors());
-//
-//
-//        int lineNumber = 0;
-//        try (FileReader fileReader = new FileReader(filePath);
-//             BufferedReader reader = new BufferedReader(fileReader)) {
-//
-//            String line;
-//            LogParser parser = new LogParser();
-//            while ((line = reader.readLine()) != null) {
-//                lineNumber++;
-//                try {
-//                    LogEntry logParseResult = parser.parseLogLine(lineNumber + ": " + line);
-//                    entries.add(logParseResult);
-//                } catch (Exception e) {
-//                    System.err.println("Ошибка парсинга строки: " + line);
-//                }
-//            }
-//        } catch (LineLengthException e) {
-//            System.err.println("Ошибка длины строки: " + e.getMessage());
-//        } catch (IOException e) {
-//            System.err.println("Ошибка при чтении файла: " + e.getMessage());
-//        } finally {
-//            long endTime = System.nanoTime();
-//            long duration = endTime - startTime;
-//            System.out.println("Время выполнения: " + duration + " наносекунд");
-//            System.out.println("Количество обработанных строк: " + lineNumber);
-//        }
-//        return entries;
-//    }
 
     public void processFile(String filePath) {
         long startTime = System.nanoTime();
@@ -134,37 +68,6 @@ public class FileProcessor {
             return null;
         }
     }
-
-//    private List<LogEntry> parseFile(String filePath) {
-//        long startTime = System.nanoTime();
-//        AtomicInteger lineNumber = new AtomicInteger(1);
-//        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
-//            return lines
-//                    .parallel()
-//                    .map(line -> {
-//                        try {
-//                            if (line.length() > LineLengthException.MAX_LINE_LENGTH) throw new LineLengthException(lineNumber.get(), line.length());
-//                            return new LogParser().parseLogLine(lineNumber.getAndIncrement() + ": " + line);
-//                        } catch (LineLengthException e) {
-//                            System.err.println("Ошибка допустимой длинны строки: " + e.getMessage());
-//                            return null;
-//                        } catch (Exception e) {
-//                            System.err.println("Ошибка обработки строки " + lineNumber.get() + ": " + line);
-//                            return null;
-//                        }
-//                    })
-//                    .filter(Objects::nonNull)
-//                    .collect(Collectors.toList());
-//        } catch (IOException e) {
-//            System.err.println("Ошибка при чтении файла: " + e.getMessage());
-//            return Collections.emptyList();
-//        } finally {
-//            long endTime = System.nanoTime();
-//            long duration = endTime - startTime;
-//            System.out.println("Время выполнения: " + duration + " наносекунд");
-//            System.out.println("Количество обработанных строк: " + lineNumber);
-//        }
-//    }
 
     public void printStatistics() {
         for (ProcessingStrategy strategy : strategies) {
